440394a40c0c877c7710225b39bf4d72d51c0d7a
doodinaveenkumar <72561458+doodinaveenkumar@users.noreply.github.com>
2020-11-19 20:16:37 +0530

    Update to consumerestapplicationtests
    Update to consumerestapplicationtests

--- src/test/java/com/example/demo/ConsumeRestApplicationTests.java
+++ src/test/java/com/example/demo/ConsumeRestApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class ConsumeRestApplicationTests {
@@ -9,5 +10,12 @@ class ConsumeRestApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	public  void Restservice() {
+		
+			RestTemplate resttemplate = new RestTemplate();
+			Greeting greeting = resttemplate.getForObject("http://localhost:8080//greetingMood?mood=1", Greeting.class);
+			System.out.println(greeting.getName());
+}
 }
