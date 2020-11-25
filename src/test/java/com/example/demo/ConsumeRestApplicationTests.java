a44ce56fa93073889c402dee536d29d6e0915ae0
c-fpossenti <filippo.possenti@almacgroup.com>
2020-11-25 14:23:00 +0000

    Add test for GreetingsController
    This commit adds basic tests for GreetingsController
    showing how to use MockMvc to perform integration
    tests in spring-boot

--- src/test/java/com/example/demo/ConsumeRestApplicationTests.java
+++ src/test/java/com/example/demo/ConsumeRestApplicationTests.java
@@ -1,13 +1,69 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ConsumeRestApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGreetingsMoodApi() throws Exception {
+		// given
+		String url = "/greetingMood";
+		String mood = "1";
+
+		// when
+		ResultActions ra = mvc.perform(
+				get(url)
+						.param("mood", mood)
+						.contentType(MediaType.APPLICATION_JSON)
+		);
+		String result = ra.andReturn().getResponse().getContentAsString();
+		Greeting data = objectMapper.readValue(result, Greeting.class);
+
+		// then
+		assertThat(data.getName(), is("Good Day!!"));
+	}
+
+	@Test
+	public void testGreetingsMoodApiReturnsOk() throws Exception {
+		// given
+		String url = "/greetingMood";
+		String mood = "1";
+
+		// when
+		ResultActions ra = mvc.perform(
+				get(url)
+						.param("mood", mood)
+						.contentType(MediaType.APPLICATION_JSON)
+		);
+
+		// then
+		ra.andExpect(status().isOk());
 	}
 
 }
